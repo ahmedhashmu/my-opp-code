08a4c56c99b733b7ec50cad1494042d4f1b4c58d
ahmed hashmi <ahmedhashmi1972@gmail.com>
2025-09-23 01:43:40 +0500

    Delete OOP directory

--- OOP/change calculator main.py
+++
@@ -1,9 +0,0 @@
-from change_maker import ChangeMaker
-
-if __name__ == "__main__":
-    charged = int(input("Enter amount charged: "))
-    given = int(input("Enter amount given: "))
-
-    changer = ChangeMaker(charged, given)  # object created
-    changer.calculate_change()
-    changer.display_change()
